6b54be31c9107eb6060e400e5eed0fda7502449f
sampu3011 <53847721+sampu3011@users.noreply.github.com>
2022-08-02 16:40:52 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] s) {
+        System.out.println("Hello all");
+        for(int i=0;i<20;i++) {
+            System.out.println("value of I :"+i);
+        }
+    }
+}
